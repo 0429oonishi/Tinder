392f64b4dd361bf8d223f010b91cbfc59a2c8a99
0429oonishi <oonishireon110@gmail.com>
2021-06-03 16:00:29 +0900

    stackViewでレイアウト作成

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        let view1 = UIView()
+        view1.backgroundColor = .green
+        let view2 = UIView()
+        view2.backgroundColor = .red
+        let view3 = UIView()
+        view3.backgroundColor = .blue
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        self.view.addSubview(stackView)
+        [view1.heightAnchor.constraint(equalToConstant: 100),
+         view3.heightAnchor.constraint(equalToConstant: 100),
+         stackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+         stackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+         stackView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+         stackView.rightAnchor.constraint(equalTo: self.view.rightAnchor)
+        ].forEach { $0.isActive = true }
+        
     }
 
 
